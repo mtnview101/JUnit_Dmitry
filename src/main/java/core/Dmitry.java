519a03fba5cc10e984f420bcf6ae6777b4dbd58d
mtnview101 <mtnview101@gmail.com>
2016-11-02 23:08:39 -0700

    second commit

---
+++ src/main/java/core/Dmitry.java
@@ -0,0 +1,13 @@
+package core;
+
+/**
+ * Hello Dmitry from Mountain View!
+ *
+ */
+public class Dmitry 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Dmitry from Mountain View!" );
+    }
+}
